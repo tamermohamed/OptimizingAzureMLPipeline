05d55ea497454b605c78184984ca389292c77e8f
tamermohamed <tamermohamed@users.noreply.github.com>
2020-11-29 02:22:54 +0300

    Add files via upload

--- train.py
+++ train.py
@@ -11,6 +11,8 @@ from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
 
+run = Run.get_context()
+
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -40,18 +42,10 @@ def clean_data(data):
     return x_df,y_df
 
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
 ds = TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
-
 x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, shuffle=True)
-
-run = Run.get_context()
 
 def main():
     # Add arguments to script
